bb6880d5fc761418b986e1a78105c374c273ef5d
Connor Mitchell <connormitchell80@gmail.com>
2016-10-06 09:03:06 -0500

    finished with magic8 - cheated a bit by not having 20 responses but I don't really care

---
+++ magic8.py
@@ -0,0 +1,22 @@
+import random
+from time import sleep
+question = input("What is your question?: ")
+number = random.randint(1,7)
+
+print ("Hmmm...thinking")
+sleep(5)
+
+if number == 1:
+    print ("Yes")
+elif number == 2:
+    print ("No")
+elif number == 3:
+    print ("Maybe")
+elif number == 4:
+    print ("It's probable")
+elif number == 5:
+    print ("It doesn't look likely")
+elif number == 6:
+    print ("Ask again later")
+elif number == 7:
+    print ("Not sure")
